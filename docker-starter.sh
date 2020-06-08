be33c359129db6e43e090aa85a8fe3522d1cf3ae
lucaschimweg <l.schimweg@gmail.com>
2020-06-08 20:06:57 +0200

    Adapt docker build to contain logout url

--- docker-starter.sh
+++ docker-starter.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo '{"apiRoot": "'$HOPPER_API_URL'", "loginUrl": "'$HOPPER_LOGIN_URL'"}' > /usr/share/nginx/html/instance.json
+echo '{"apiRoot": "'$HOPPER_API_URL'", "loginUrl": "'$HOPPER_LOGIN_URL'", "logoutUrl": "'$HOPPER_LOGOUT_URL'"}' > /usr/share/nginx/html/instance.json
 
 nginx -g "daemon off;"
